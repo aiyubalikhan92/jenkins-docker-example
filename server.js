edd4423b105c9e1b86cdfcfdce5fa628a3be8285
aiyubalikhan92 <aiyubalikhan92@gmail.com>
2023-02-06 17:27:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello fathimaa');
+  res.send('jenkins docket sample - test');
 });
 
 app.listen(PORT, HOST);
